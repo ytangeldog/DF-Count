7fbce47255b6f9a2d06d0e80dfc90f998906576d
ytangeldog2145 <ahmedkmh2000@gmail.com>
2024-06-03 21:06:03 +0300

    Update update.py

--- stats/update.py
+++ stats/update.py
@@ -1,6 +1,6 @@
 with open('stats/temp.txt', 'r') as file:
   tempnum = file.read().strip()
 with open('stats/stats.txt', 'a') as file:
-  file.write(f'\n{tempnum}')
+  file.write(f'{tempnum}\n')
 with open('stats/temp.txt', 'w') as file:
   tempnum = file.write('0')
