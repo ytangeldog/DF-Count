3fdde241ccdf27c8f506d5054ea81bbf624408ab
ytangeldog2145 <ahmedkmh2000@gmail.com>
2024-06-03 20:25:51 +0300

    Create graphmaker.py

---
+++ dfconnected/stats/graphmaker.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+# Function to read the stats.txt file
+def read_stats(filename):
+    with open(filename, 'r') as file:
+        data = file.readlines()
+    return [float(item.strip()) for item in data if item.strip()]
+
+# Function to create the graph
+def create_graph(data):
+    last_seven_items = data[-7:]
+    labels = [f'day{i+1}' for i in range(len(last_seven_items))]
+    
+    plt.figure(figsize=(10, 5))
+    plt.plot(labels, last_seven_items, marker='o')
+    plt.title('Df classic players week stats')
+    plt.xlabel('Days')
+    plt.ylabel('Players')
+    plt.grid(True)
+    plt.savefig('dfconnected/graph.png')
+    plt.show()
+
+
+filename = 'dfconnected/stats/stats.txt'
+data = read_stats(filename)
+create_graph(data)
