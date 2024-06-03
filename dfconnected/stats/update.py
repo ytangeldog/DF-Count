ac98b58d46e1b2de3f42048a1a136da457ff83fd
ytangeldog2145 <ahmedkmh2000@gmail.com>
2024-06-03 21:05:43 +0300

    Update update.py

--- dfconnected/stats/update.py
+++ dfconnected/stats/update.py
@@ -1,6 +1,6 @@
 with open('dfconnected/stats/temp.txt', 'r') as file:
   tempnum = file.read().strip()
 with open('dfconnected/stats/stats.txt', 'a') as file:
-  file.write(f'\n{tempnum}')
+  file.write(f'{tempnum}\n')
 with open('dfconnected/stats/temp.txt', 'w') as file:
   tempnum = file.write('0')
