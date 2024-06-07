1cde08bf4448483e903a17792081453b61c5153f
ytangeldog2145 <ahmedkmh2000@gmail.com>
2024-06-08 00:30:37 +0300

    Update graphmaker.py

--- stats/graphmaker.py
+++ stats/graphmaker.py
@@ -14,9 +14,14 @@ def create_graph(data):
     labels = [f'{(day + timedelta(i)).strftime('%A')}' for i in range(len(last_seven_items))]
     
     fig, ax = plt.subplots(figsize=(10, 5))
-    ax.plot(labels, last_seven_items, marker='o')
+    ax.plot(labels, last_seven_items, marker='o', markersize=12)
     for i, value in enumerate(last_seven_items):
-        ax.text(labels[i], value, str(int(value)), ha='center', fontsize=15)
+        x = i
+        y = value
+        text = str(int(value))
+        bbox_props = dict(boxstyle="circle,pad=0.2", lw=0)
+        ax.text(x, y, text, ha="center", va="center", size=10,color='white',fontweight='bold',
+                bbox=bbox_props)
     ax.set_title('Df classic players week stats')
     ax.set_xlabel('Days')
     ax.set_ylabel('Players')
